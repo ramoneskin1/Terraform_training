2350f1e36ef4d3a34d9ae2b80b99e9192a218f9f
Rhernandez <ramon.hernandez987@gmail.com>
2026-02-25 23:00:04 -0600

    Update createInstance.tf

--- createInstance.tf
+++ createInstance.tf
@@ -1,10 +1,4 @@
-provider "aws"{
-	access_key = "AKIAUT2C35BWJVFCOXH5"
-	secret_key = "secret0key-here"
-	region     = "us-east-2"
-}
-
 resource "aws_instance" "MyterraformInstance" {
 	ami 			= "ami-05803413c51f242b7"
-	instance_type   = "t2.micro"
+	instance_type   = "t3.micro"
 }
