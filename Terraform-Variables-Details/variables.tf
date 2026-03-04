6105e5b0d1b9eed87bbeb1205b9b63a506ffbaeb
Rhernandez <ramon.hernandez987@gmail.com>
2026-03-03 23:17:48 -0600

    Update variables.tf
    amis

--- Terraform-Variables-Details/variables.tf
+++ Terraform-Variables-Details/variables.tf
@@ -10,3 +10,12 @@ variable "Security_Group" {
   type = "list"
   default = ["sg-24976","sg-90890","sg-456789"]
 }
+variable "AMIS" {
+  type = map
+  default = {
+    us-east-1 = "ami-0b0ea68c435eb488d"
+    us-east-2 = "ami-05803413c51f242b7"
+    us-west-1 = "ami-0454207e5367abf01"
+    us-west-2 = "ami-0688ba7eeeeefe3cd"
+ }
+}
