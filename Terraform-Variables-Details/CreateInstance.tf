3810d4f110a28a5de253d9cd7578fd52b8ea6f0d
Rhernandez <ramon.hernandez987@gmail.com>
2026-03-04 18:08:38 -0600

    Update CreateInstance.tf
    provision software

--- Terraform-Variables-Details/CreateInstance.tf
+++ Terraform-Variables-Details/CreateInstance.tf
@@ -1,10 +1,35 @@
+
+resource "aws_key_pair" "levelup_key" {
+	key_name = "levelup_key"
+	public_key = file(var.PATH_TO_PUBLIC_KEY)
+}
+
 resource "aws_instance" "MyterraformInstance" {
 	ami 			= lookup(var.AMIS, var.AWS_REGION)
 	instance_type   = "t3.micro"
+	key_name = aws_key_pair.levelup_key.key_name
 
 	tags = {
-		Name = "demoinstance"
+		Name = "my_custom_instance"
 	}
 
-security_groups ="${var.Security_Group}"
+provisioner "file" {
+	source = "installNginx.sh"
+	destination = "/tmp/installNginx.sh"
+}
+
+provisioner "remote-exec" {
+	inline = [
+	"chmod +x /tmp/installNginx.sh",
+	"sudo sed -i -e 's/\r$//' /tmp/installNginx.sh, # Remove the spurious CR characters
+	"sudo /tmp/installNginx.sh",
+ ]
+}
+
+connection {
+	host 		= coalesce(self.public_ip, self.private_ip)
+	type		= "ssh"
+	user		= var.INSTANCE_USERNAME
+	private_key = file(var.PATH_TO_PRIVATE_KEY)
+ }
 }
