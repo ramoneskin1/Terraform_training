0eac1e9eb0f4f6768a67044abc2e4bebbee47bf4
Rhernandez <ramon.hernandez987@gmail.com>
2026-03-11 17:51:37 -0600

    Update variables.tf

--- Terraform-Variables-Details/Demo-DataSource/variables.tf
+++ Terraform-Variables-Details/Demo-DataSource/variables.tf
@@ -15,7 +15,7 @@ variable "Security_Group" {
 }
 
   variable "AMIS" {
-    typer = map
+    type = map
     default = {
       us-west-2 = "ami-0688ba7eeeeefe3cd"
       us-west-1 = "ami-0454207e5367abf01"
