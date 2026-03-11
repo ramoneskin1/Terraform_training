80ee092dec9c4536bc92a6addc7176c1430978d4
Rhernandez <ramon.hernandez987@gmail.com>
2026-03-11 17:09:43 -0600

    Create createInstance.tf

---
+++ Terraform-Variables-Details/Demo-DataSource/createInstance.tf
@@ -0,0 +1,30 @@
+
+data "aws_availability_zones" "available" {}
+
+data "aws_ami" "latest-ubuntu" {
+  most_recent = true
+  owners = ["099720109477"]
+
+  filter {
+    name "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+
+resource "aws_instance" "MyInstance" {
+  ami           = data.aws_ami.latest-ubuntu.id
+  instance_type = t3.micro
+  availability_zone = data.aws_availability_zones.available.names [1]
+
+  tags = {
+    Name = "custom_instance"
+  }
+
+}
+ 
