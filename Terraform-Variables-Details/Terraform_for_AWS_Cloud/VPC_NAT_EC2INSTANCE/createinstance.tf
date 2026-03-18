d9c53482e8186373004d7c1d30f7518899d224a0
Rhernandez <ramon.hernandez987@gmail.com>
2026-03-18 00:05:14 -0600

    Update createinstance.tf

--- Terraform-Variables-Details/Terraform_for_AWS_Cloud/VPC_NAT_EC2INSTANCE/createinstance.tf
+++ Terraform-Variables-Details/Terraform_for_AWS_Cloud/VPC_NAT_EC2INSTANCE/createinstance.tf
@@ -8,10 +8,10 @@ resource "aws_instance" "MyterraformInstance" {
 	ami 			= lookup(var.AMIS, var.AWS_REGION)
 	instance_type   = "t3.micro"
 	key_name = aws_key_pair.levelup_key.key_name
-}
+
 	vpc_security_group_ids = [aws_security_group.allow-levelup-ssh.id]
 	subnet_id = aws_subnet.levelupvpc-public-2.id
-
+}
 	tags = {
 		Name = "my_custom_instance"
 	}
