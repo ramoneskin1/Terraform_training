4fe21b4caaaaaa4b729a710638e56e23fd2512a6
Rhernandez <ramon.hernandez987@gmail.com>
2026-03-25 23:56:06 -0600

    Update security_group.tf

--- Terraform-Variables-Details/Terraform-for-AWS-II/AWS-RDS/security_group.tf
+++ Terraform-Variables-Details/Terraform-for-AWS-II/AWS-RDS/security_group.tf
@@ -23,3 +23,28 @@ resource "aws_security_group" "allow-levelup-ssh" {
   }
 }
 
+#Security group for MariaDB
+resource "aws_security_group" "allow-mariadb" {
+  name        = "allow-mariadb"
+  description = "security group mariaDB"
+  vpc_id      = aws_vpc.levelup_vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port  = 3306
+      to_port    = 3306
+      protocol   = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+      security_groups = [aws_security_group.allow-levelup-ssh.id]
+      }
+      
+  tags = {
+    Name = "allow-mariadb"
+  }
+}
