344fafb0150e8298b900e92b864c359427fe88b0
Rhernandez <ramon.hernandez987@gmail.com>
2026-03-25 23:39:24 -0600

    Create variables.tf

---
+++ Terraform-Variables-Details/Terraform-for-AWS-II/AWS-RDS/variables.tf
@@ -0,0 +1,33 @@
+variable "AWS_ACCESS_KEY" {}
+
+variable "AWS_SECRET_KEY" {}
+
+variable "AWS_REGION" {
+  default = "us-east-2"
+}
+
+variable "Security_Group" {
+  type = list
+  default = ["sg-24976","sg-90890","sg-456789"]
+}
+variable "AMIS" {
+  type = map
+  default = {
+    us-east-1 = "ami-0b0ea68c435eb488d"
+    us-east-2 = "ami-05803413c51f242b7"
+    us-west-1 = "ami-0454207e5367abf01"
+    us-west-2 = "ami-0688ba7eeeeefe3cd"
+ }
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "levelup_key"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "levelup_key.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+  default ="ubuntu"
+}
