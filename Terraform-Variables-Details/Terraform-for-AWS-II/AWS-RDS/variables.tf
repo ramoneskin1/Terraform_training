770fdb325ae9eb191f78f7e4bdcdadf4353f28d0
Rhernandez <ramon.hernandez987@gmail.com>
2026-03-25 23:47:07 -0600

    Update variables.tf

--- Terraform-Variables-Details/Terraform-for-AWS-II/AWS-RDS/variables.tf
+++ Terraform-Variables-Details/Terraform-for-AWS-II/AWS-RDS/variables.tf
@@ -6,10 +6,6 @@ variable "AWS_REGION" {
   default = "us-east-2"
 }
 
-variable "Security_Group" {
-  type = list
-  default = ["sg-24976","sg-90890","sg-456789"]
-}
 variable "AMIS" {
   type = map
   default = {
