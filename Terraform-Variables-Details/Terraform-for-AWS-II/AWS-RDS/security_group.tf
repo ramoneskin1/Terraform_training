8e44bec042f2b084727c54f60ef7e27f3a07536c
Rhernandez <ramon.hernandez987@gmail.com>
2026-03-25 23:49:40 -0600

    Update security_group.tf

--- Terraform-Variables-Details/Terraform-for-AWS-II/AWS-RDS/security_group.tf
+++ Terraform-Variables-Details/Terraform-for-AWS-II/AWS-RDS/security_group.tf
@@ -1,1 +1,25 @@
-#
+#Security Group for levelupvpc
+resource "aws_security_group" "allow-levelup-ssh" {
+  name        = "allow-levelup-ssh"
+  description = "security group that allows ssh connection"
+  vpc_id      = aws_vpc.levelup_vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      from_port  = 22
+      to_port    = 22
+      protocol   = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+      }
+      
+  tags = {
+    Name = "allow-levelup-ssh"
+  }
+}
+
