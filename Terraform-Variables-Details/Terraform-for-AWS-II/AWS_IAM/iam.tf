a3bdd5b245a9045fb953357090b8f33e772242ed
Rhernandez <ramon.hernandez987@gmail.com>
2026-03-30 23:30:14 -0600

    Update iam.tf

--- Terraform-Variables-Details/Terraform-for-AWS-II/AWS_IAM/iam.tf
+++ Terraform-Variables-Details/Terraform-for-AWS-II/AWS_IAM/iam.tf
@@ -1,1 +1,36 @@
-#iam file
+#file for IAM users and groups
+
+resource "aws_iam_user" "adminuser1" {
+  name = "adminuser1"
+}
+
+resource "aws_iam_user" "adminuser1" {
+  name = "adminuser1"
+}
+
+# group TF definition
+resource "aws_iam_group" "admingroup" {
+  name = "admingroup"
+}
+
+#Assign user to AWS group
+resource "aws_iam_group_memebership" "admin-user" {
+  name = "admin-users"
+  users = [
+  aws_iam_user.adminuser1.name,
+  aws_iam_user.adminuser2.name,
+]
+  group = aws_iam_group.admingroup.name
+}
+
+#Policy for AWS Group
+resource "aws_iam_policy_attachment" "admin-users-attach" {
+  name  = "admin-users-attach"
+  groups = [aws_iam_group.admingroup.name]
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+
+
+
+  
