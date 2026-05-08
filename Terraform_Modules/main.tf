2c4d583435e54f5e4575e401f919dd2dce600dc2
Rhernandez <ramon.hernandez987@gmail.com>
2026-05-07 23:21:22 -0600

    Update main.tf

--- Terraform_Modules/main.tf
+++ Terraform_Modules/main.tf
@@ -1,1 +1,12 @@
-#main
+module "ec2_cluster" {
+  source       = "github.com/terraform-aws-modules/terraform-aws-ec2-instance"
+  name         = "my-cluster"
+  ami          = "ami-05cf1e9f73fbad2e2"
+ instance_type = "t3.micro"
+ subnet_id     = "subnet-c92f9cc8 " 
+
+tags = {
+Terraform   = "true"
+Environment = "dev"
+ }
+}
